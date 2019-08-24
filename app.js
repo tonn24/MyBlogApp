00ffba3065cbb9ecb107ef0823b70f791907ea59
Tõnn <tonn.toomsalu@gmail.com>
2019-08-24 15:22:12 +0300

    Index createtion

--- app.js
+++ app.js
@@ -4,14 +4,34 @@ const express = require("express"),
       bodyParser = require("body-parser"),
       PORT = 3000;
 
-
+//APP CONFIGURATION
 mongoose.connect("mongodb://localhost/myBlogApp", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Mongoose model configuration
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//ROUTES
 app.get("/", (req, res) => {
-    res.send("Home page!")
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if(err){
+            console.log("ERROR!")
+        } else {
+            res.render("index", {blogs: blogs}); 
+        }
+    });
 });
 
 app.listen(PORT, () => {
