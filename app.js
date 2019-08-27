dda71ade00fd4efb03e3a229ce8f7441868f78b7
Tõnn <tonn.toomsalu@gmail.com>
2019-08-27 15:01:02 +0300

    Update route creation

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require("express"),
+      app = express(),
+      mongoose = require("mongoose"),
+      bodyParser = require("body-parser"),
+      PORT = 3000,
+      methodOverride = require("method-override");
+
+//APP CONFIGURATION
+mongoose.connect("mongodb://localhost/myBlogApp", { useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+//Mongoose model configuration
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//ROUTES
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if(err){
+            console.log("ERROR!")
+        } else {
+            res.render("index", {blogs: blogs}); 
+        }
+    });
+});
+
+//NEW ROUTE
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+});
+//CREATE ROUTE
+app.post("/blogs", (req, res) => {
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if(err){
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", (req, res) => {
+	Blog.findById(req.params.id, (err, foundBlog) => {
+		if(err){
+			res.redirect("/blogs");
+		} else{
+			res.render("show", {blog: foundBlog});
+		}
+	});
+});
+
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res) => {
+	Blog.findById(req.params.id, (err, foundBlog) => {
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, foundBlog) => {
+        if(err){
+            res.redirect("/blogs");
+        }  else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log("Server is running localhost:" + PORT );
+});
+
